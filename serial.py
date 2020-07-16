4e6eac7a269f1c7ab1fb07a419d4ac61a9e3588d
heyimmatt <mrmcfarland@gmail.com>
2020-07-16 11:24:33 -0700

    Add class initializor and generate function

--- serial.py
+++ serial.py
@@ -19,4 +19,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create SerialGenerator with a starting number"""
 
+        self.start = start
+        self.counter = 0
+
+    def generate(self):
+        """Generate a serial number that increments 1 each time"""
+        
+        if self.counter == 0:
+            self.counter += 1
+            return self.start
+        else:
+            self.counter += 1
+            self.start += 1
+            return self.start
+        
+    
